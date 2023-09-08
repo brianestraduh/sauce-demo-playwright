239b804b7f101d780b15a6f0863309d836d275e3
Brian Estrada <bestr2015@gmail.com>
2023-09-07 21:28:06 -0700

    finshed testing inventory page

--- e2e/fixtures/pages/inventory-page.ts
+++ e2e/fixtures/pages/inventory-page.ts
@@ -1,4 +1,6 @@
-import {type Locator, type Page, expect} from '@playwright/test'
+import {type Locator, type Page, expect, test} from '@playwright/test'
+
+
 
 export class InventoryPage {
     //variables
@@ -6,6 +8,10 @@ export class InventoryPage {
     readonly inventoryList: Locator;
     readonly inventoryItems: Locator;
     readonly sortButton: Locator;
+    readonly inventoryItemTitles: Locator;
+    readonly itemNames: Array<string>;
+    readonly sortedItemNames: Array<string>;
+    readonly itemPrices: Locator;
 
     //constructor
     constructor(page: Page) {
@@ -13,6 +19,8 @@ export class InventoryPage {
         this.inventoryList = this.page.locator('.inventory_list');
         this.inventoryItems = this.page.locator('.inventory_item');
         this.sortButton = this.page.locator('.product_sort_container');
+        this.inventoryItemTitles = this.page.locator('.inventory_item_name');
+        this.itemPrices = this.page.locator('.inventory_item_price');
     }
 
     //methods
@@ -20,5 +28,54 @@ export class InventoryPage {
         await expect(this.inventoryList).toBeVisible();
         await expect(this.inventoryItems).toHaveCount(6);
     }
+    async SortByNameAtoZ() {
+        await this.sortButton.selectOption({label: 'Name (A to Z)'});
+            const itemNames = await this.page.$$eval('.inventory_item_name', (items) => {
+            return items.map(item => item.textContent);
+        });
+        const sortedItemNames = [...itemNames].sort();
+        console.log(itemNames);
+        //expec to not equal
+        expect(itemNames).toEqual(sortedItemNames);
+        console.log(sortedItemNames);
+    }
+
+    async SortByNameZtoA() {
+        await this.sortButton.selectOption({label: 'Name (Z to A)'});
+            const itemNames = await this.page.$$eval('.inventory_item_name', (items) => {
+            return items.map(item => item.textContent);
+        });
+        console.log(itemNames);
+        const sortedItemNames = [...itemNames].sort().reverse();
+        //expec to not equal
+        expect(itemNames).toEqual(sortedItemNames);
+        console.log(sortedItemNames);
+    }
+
+    async SortByPriceLowToHigh() {
+        await this.sortButton.selectOption({label: 'Price (low to high)'});
+        const itemPrices = await this.page.$$eval('.inventory_item_price', (items) => {
+            return items.map(item => item.textContent);
+        });
+        //sort itemPrices array from low to high
+        const sortedItemPrices = [...itemPrices].sort((a, b) => a - b);
+        //expect item prices to equal sorted item prices
+        console.log(itemPrices, sortedItemPrices);
+        expect(itemPrices).toEqual(sortedItemPrices);
+        
+        
+    }
+    async SortByPriceHighToLow() {
+        await this.sortButton.selectOption({label: 'Price (high to low)'});
+        const itemPrices = await this.page.$$eval('.inventory_item_price', (items) => {
+            return items.map(item => item.textContent);
+        });
+        const sortedItemPrices = [...itemPrices].sort((a, b) => b - a);
+        //expect item prices to equal sorted item prices
+        console.log(itemPrices, sortedItemPrices);
+        expect(itemPrices).toEqual(sortedItemPrices);
+        
+    }
+    
 
 }
--- tests/inventory.spec.ts
+++ tests/inventory.spec.ts
@@ -10,10 +10,21 @@ test.describe('Inventory Page tests', () => {
         inventoryPage.InventoryPageLoads();
    
     });
-    //implement test for sort by price, high to low
-    // implement test for sort by price, low to high
     //implement test for sort by A-Z
+    test('should sort by name, A-Z', async ({inventoryPage}) => {
+        inventoryPage.SortByNameAtoZ();
+    });
     //implement test for sort by Z-A
-
+    test('should sort by name Z-A', async ({inventoryPage}) => {
+        inventoryPage.SortByNameZtoA();
+    });
+    // implement test for sort by price, low to high
+    test('should sort by price, low to high', async ({inventoryPage}) => {
+        inventoryPage.SortByPriceLowToHigh();
+    });
+    // implement test for sort by price, high to low
+    test('should sort by price, high to low', async ({inventoryPage}) => {
+        inventoryPage.SortByPriceHighToLow();
+    });
 
 });
